b54ec789efcf6d4732cf6de3be0fb42c466d86d4
Anton Barbakov <anton.barbakov@globallogic.com>
2021-05-12 10:33:36 +0300

    page-object pattern update

--- test/page-objects/App.js
+++ test/page-objects/App.js
@@ -2,6 +2,14 @@ class App {
     openHomePage () {
         browser.url('http://zero.webappsecurity.com/index.html')
     }
+
+    openLoginPage() {
+        browser.url('http://zero.webappsecurity.com/login.html')
+    }
+
+    logout() {
+        browser.url('http://zero.webappsecurity.com/logout.html')
+    }
 }
 
 export default new App()
