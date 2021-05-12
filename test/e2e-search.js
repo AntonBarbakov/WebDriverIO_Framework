cce93bc991d1bec7f605655590a0ef6be3e9754f
Anton Barbakov <anton.barbakov@globallogic.com>
2021-05-12 19:45:55 +0300

    v0.0.5

--- test/e2e-search.js
+++ test/e2e-search.js
@@ -1,8 +1,14 @@
+import App from '../page-objects/App'
+import LoginPage from '../page-objects/pages/LoginPage'
+import ExchangePage from '../page-objects/pages/ExchangePage'
+import Navbar from '../page-objects/components/Navbar'
+
 describe('E2E Tests - Search', () => {
-	it('Should load homepage', () => {
-		browser.url('http://zero.webappsecurity.com/index.html')
-		$('#searchTerm').waitForExist()
-	})
+	it('Should log into application', () => {
+        App.openLoginPage()
+        LoginPage.login('username', 'password')
+        Navbar.insideNavbarIsVisible()
+    })
  
 	it('Should submit searchbox', () => {
 		$('#searchTerm').setValue('bank')
@@ -10,35 +16,5 @@ describe('E2E Tests - Search', () => {
 		const resultsTitle = $('h2')
 		resultsTitle.waitForExist()
 		expect(resultsTitle).toHaveText('Search Results:')
-	})
-})
- 
-describe('E2E Tests - Currency Exchange', () => {
-	it('Should log into application', () => {
-		browser.url('http://zero.webappsecurity.com/index.html')
-		$('#signin_button').waitForExist()
-		$('#signin_button').click()
-		$('#login_form').waitForExist()
-		$('#user_login').setValue('username')
-		$('#user_password').setValue('password')
-		$('input[type="submit"]').click()
-		$('.nav-tabs').waitForExist()
-	})
- 
-	it('Should make currency exchange', () => {
-		$('#pay_bills_tab').waitForExist()
-		$('#pay_bills_tab').click()
-		$('#tabs > ul > li:nth-child(3) > a').waitForExist()
-		$('#tabs > ul > li:nth-child(3) > a').click()
-		const currencySelect = $('#pc_currency')
-		currencySelect.waitForExist()
-		currencySelect.selectByAttribute('value', 'GBP')
-		$('#pc_amount').setValue('500')
-		$('#pc_inDollars_true').click()
-		$('#purchase_cash').click()
-		const message = $('#alert_content')
-		expect(message).toHaveText(
-			'Foreign currency cash was successfully purchased.'
-		)
 	})
 })
